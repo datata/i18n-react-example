bc8614a58113135849ef4eecdacc9d9958dd16ea
datata <dani@danitarazona.dev>
2024-04-03 16:26:45 +0200

    feat: export i18n config to own folder config

---
+++ src/i18n/i18nConfig.ts
@@ -0,0 +1,20 @@
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import translation_en from "./locales/en/translation.json";
+import translation_es from "./locales/es/translation.json";
+
+i18next
+  // .use(initReactI18next) 
+  .init({resources: {
+      en: translation_en,
+      es: translation_es
+    },
+    lng: "en",
+    fallbackLng: "en",
+
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+  export default i18next;
