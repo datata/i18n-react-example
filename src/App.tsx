11d7aca830ce1b33ff84de15bd8e0d2595076c57
datata <dani@danitarazona.dev>
2024-04-03 15:14:01 +0200

    feat: i18n working

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
+import { useTranslation } from 'react-i18next';
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const { t, i18n } = useTranslation();
+  const [selectedLanguage, setSelectedLanguage] = useState("en");
+
+  const selectLanguageHandler = (e: any) => {
+    const selectedLanguage = e.target.value
+    setSelectedLanguage(selectedLanguage);
+
+    i18n.changeLanguage(selectedLanguage)
+  }
+
   return (
     <>
       <div>
-        <h1>Welcome</h1>
+        <header>
+          <select onChange={selectLanguageHandler} name="Select Language" id="" value={selectedLanguage}>
+            <option value="Select Language">Select Language</option>
+            <option value="es">Es</option>
+            <option value="en">En</option>
+          </select>
+        </header>
+        <h1>{t('welcome')}</h1>
       </div>
     </>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { I18nextProvider, initReactI18next } from 'react-i18next';
+import i18n from 'i18next';
+import i18next from 'i18next';
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({resources: {
+      en: {
+        translation: {
+          "welcome": "Welcome"
+        }
+      },
+      es: {
+        translation: {
+          "welcome": "Bienvenidos"
+        }
+      }
+    },
+    lng: "en",
+    fallbackLng: "en",
+
+    interpolation: {
+      escapeValue: false
+    }
+  });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>,
 )
